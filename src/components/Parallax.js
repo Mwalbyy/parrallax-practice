9db51bc62855547458822cc0f2f5d445304b540d
Marcus Walby <marcuswalby24@gmail.com>
2022-05-10 17:55:33 -0400

    added parallax

---
+++ src/components/Parallax.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
+export default function Parallax() {
+  return (
+<>
+<Parallax pages={6}>
+    <ParallaxLayer ></ParallaxLayer>
+</Parallax>
+</>
+    )
+}
